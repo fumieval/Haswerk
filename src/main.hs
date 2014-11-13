0077582b43d1d6391ba2b5df39e6497fd0a4b9a4
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-11-13 15:30:55 +0900

    objective-0.4.1 change

--- src/main.hs
+++ src/main.hs
@@ -11,41 +11,34 @@ import Assets
 import Util
 import Control.Lens
 
-pipeline :: Address (State w) (System s) -> Object e (StateT w (System s)) -> Object e (System s)
-pipeline addr obj = Object $ \e -> do
-  s <- addr .& get
-  ((a, o'), s') <- runStateT (runObject obj e) s
-  addr .& put s'
-  return (a, pipeline addr o')
-
 main = runSystemDefault $ do
   setFPS 30
-  miku <- liftIO $ fromPMD "C:/Users/Fumiaki/Documents/Lat_miku/Normal.pmd"
   world <- new $ variable newWorld
   pl <- new $ pipeline world Player.object
   
-  newJoypad $ liftO $ accept $ \case
-    PadButton _ (Down 7) -> pl .^> Player.Attack
-    PadButton _ (Down 5) -> pl .^> Player.Act
-    PadButton _ (Down 4) -> pl .& Player.position' -= V3 0 1 0
-    PadButton _ (Down 6) -> pl .& Player.position' += V3 0 1 0
-    PadButton _ (Down i) -> liftIO $ print i
+  linkGamepad $ \case
+    PadButton _ (Down 7) -> pl .^ Player.Attack
+    PadButton _ (Down 5) -> pl .^ Player.Act
     _ -> return ()
   
-  newGraphic $ liftO $ accept $ \dt -> do
-    pl .^> Player.Update dt
+  linkGraphic $ \dt -> do
+    pl .^ Player.Update dt
     pos <- pl .& use Player.position
-    psp <- pl .^> Player.GetPerspective
+    psp <- pl .^ Player.GetPerspective
 
     (p:_) <- getGamepads
     (mx : mz : px : py : _) <- gamepadAxes p
+    bts <- gamepadButtons p
 
-    pl .^> Player.Move (play 0.01 $ V2 mx mz ^* dt)
-    pl .^> Player.Turn (play 0.01 $ V2 px py ^* dt)
+    when (bts !! 4) $ pl .& Player.position' += V3 0 (3*dt) 0
+    when (bts !! 6) $ pl .& Player.position' -= V3 0 (3*dt) 0
+
+    pl .^ Player.Move (play 0.002 $ V2 mx mz ^* dt)
+    pl .^ Player.Turn (play 0.002 $ V2 px py ^* dt)
     
     w <- world .& get
     return $ mconcat
-      [ psp $ mconcat [renderWorld pos w, miku]
+      [ psp $ renderWorld pos w
       , crosshair]
   
   stand
