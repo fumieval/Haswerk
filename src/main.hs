fa9b197955ca11fed93bc76e157442d7290d8951
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-11-13 15:38:05 +0900

    uniformalize method operators

--- src/main.hs
+++ src/main.hs
@@ -5,7 +5,6 @@ import World
 import Render
 import Control.Monad.Trans
 import Util
-import Control.Monad.State.Strict
 import Data.Monoid
 import Assets
 import Util
@@ -23,22 +22,20 @@ main = runSystemDefault $ do
   
   linkGraphic $ \dt -> do
     pl .^ Player.Update dt
-    pos <- pl .& use Player.position
-    psp <- pl .^ Player.GetPerspective
 
     (p:_) <- getGamepads
     (mx : mz : px : py : _) <- gamepadAxes p
     bts <- gamepadButtons p
 
-    when (bts !! 4) $ pl .& Player.position' += V3 0 (3*dt) 0
-    when (bts !! 6) $ pl .& Player.position' -= V3 0 (3*dt) 0
+    when (bts !! 4) $ pl .^ Player.position' += V3 0 (3*dt) 0
+    when (bts !! 6) $ pl .^ Player.position' -= V3 0 (3*dt) 0
 
     pl .^ Player.Move (play 0.002 $ V2 mx mz ^* dt)
     pl .^ Player.Turn (play 0.002 $ V2 px py ^* dt)
     
-    w <- world .& get
-    return $ mconcat
-      [ psp $ renderWorld pos w
-      , crosshair]
-  
+    w <- world .^ get_
+    psp <- pl .^ Player.GetPerspective
+    pos <- pl .^ use Player.position
+    return $ mconcat [psp $ renderWorld pos w, crosshair]
+
   stand
